1f24ff7635af568a470463dc658f768ce9b84558
nikola-kovrizhny <106247818+nikola-kovrizhny@users.noreply.github.com>
2024-12-16 11:35:35 +0300

    diff ip_len, ip_ttl, tcp_seqack, tcp_win

--- py/encoder.py
+++ py/encoder.py
@@ -8,15 +8,27 @@ class Context:
         self.ts1 = 0
         self.ts2 = 0
 
+        self.ip_len = defaultdict(lambda: 0)
         self.ip_id = defaultdict(lambda: 0)
+        self.ip_ttl = defaultdict(lambda: 0)
         self.ip_cs = defaultdict(lambda: 0)
+
+        self.tcp_seq = defaultdict(lambda: 0)
+        self.tcp_ack = defaultdict(lambda: 0)
+        self.tcp_win = defaultdict(lambda: 0)
 
 
 class Rules:
     PCAP_TS1_DIFF = True
     PCAP_TS2_DIFF = True
+
+    IP_LEN_DIFF = True
     IP_ID_DIFF = True
+    IP_TTL_DIFF = True
     IP_CS_DIFF = True
+
+    TCP_SEQACK_DIFF = True
+    TCP_WIN_DIFF = True
 
 
 class Encoder:
@@ -58,16 +70,28 @@ class Encoder:
             tl_off = ip_off + 4 * ip_ihl
             ip_proto = p[ip_off+9]
 
+            ip_len = big2i(p[ip_off+2:ip_off+4])
             ip_id = big2i(p[ip_off+4:ip_off+6])
+            ip_ttl = p[ip_off+8]
             ip_cs = big2i(p[ip_off+10:ip_off+12])
 
             ip_srcaddr = p[ip_off+12:ip_off+16]
             ip_dstaddr = p[ip_off+16:ip_off+20]
 
+            if Rules.IP_LEN_DIFF:
+                d = ip_len - self.ctx.ip_len[ip_srcaddr]
+                self.ctx.ip_len[ip_srcaddr] = ip_len
+                self.p[ip_off+2:ip_off+4] = i2lit(d, 2)
+
             if Rules.IP_ID_DIFF:
                 d = ip_id - self.ctx.ip_id[ip_srcaddr]
                 self.ctx.ip_id[ip_srcaddr] = ip_id
                 self.p[ip_off+4:ip_off+6] = i2lit(d, 2)
+
+            if Rules.IP_TTL_DIFF:
+                d = ip_ttl - self.ctx.ip_ttl[ip_srcaddr]
+                self.ctx.ip_ttl[ip_srcaddr] = ip_ttl
+                self.p[ip_off+8:ip_off+9] = i2lit(d, 1)
 
             if Rules.IP_CS_DIFF:
                 d = ip_cs - self.ctx.ip_cs[ip_srcaddr]
@@ -78,5 +102,25 @@ class Encoder:
                 tcp_d_off = 4 * (p[tl_off+12] // 16)
                 tcp_srcport = p[tl_off:tl_off+2]
                 tcp_drcport = p[tl_off+2:tl_off+4]
+                tcp_seq = big2i(p[tl_off+4:tl_off+8])
+                tcp_ack = big2i(p[tl_off+8:tl_off+12])
+                tcp_win = big2i(p[tl_off+14:tl_off+16])
+
+                session = (ip_srcaddr, ip_dstaddr, ip_proto,
+                           tcp_srcport, tcp_drcport)
+
+                if Rules.TCP_SEQACK_DIFF:
+                    d = tcp_seq - self.ctx.tcp_seq[session]
+                    self.ctx.tcp_seq[session] = tcp_seq
+                    self.p[tl_off+4:tl_off+8] = i2lit(d, 4)
+
+                    d = tcp_ack - self.ctx.tcp_ack[session]
+                    self.ctx.tcp_ack[session] = tcp_ack
+                    self.p[tl_off+8:tl_off+12] = i2lit(d, 4)
+
+                if Rules.TCP_WIN_DIFF:
+                    d = tcp_win - self.ctx.tcp_win[session]
+                    self.ctx.tcp_win[session] = tcp_win
+                    self.p[tl_off+14:tl_off+16] = i2lit(d, 2)
 
         return self.p
